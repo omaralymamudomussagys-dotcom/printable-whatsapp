55af143799e185a3f99e659ef7afd3ac4b202399
alyMussagy <alyibraimomussagy@gmail.com>
2026-06-01 02:43:17 +0200

    fix(express): Aumentar limite do payload JSON para 1MB

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import QRCode from 'qrcode';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
 // ─── Config ──────────────────────────────────────────────────────────────────
 const PORT = process.env.PORT || 3000;
